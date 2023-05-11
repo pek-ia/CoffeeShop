5b534aeab4fc41fc4d79ac2597f0b0d3c6c08d4a
Paul Kimball <pekimball@interfaceassociates.net>
2023-05-11 10:24:14 -0400

    initial commit

---
+++ src/main/java/CoffeeShop.java
@@ -0,0 +1,21 @@
+public class CoffeeShop {
+
+    public static void main(String[] args) {
+        CoffeeShop shop = new CoffeeShop();
+        shop.order(DrinkType.COFFEE, SizeType.MEDIUM);
+    }
+
+    double order(DrinkType beverage, SizeType size){
+        double price = 0.0;
+        switch (size){
+            case LARGE -> {price = 6.0;}
+            case MEDIUM -> {price = 4.98;}
+            case SMALL -> {price = 8.0;}
+        }
+
+        System.out.println("you ordered a " + size + " " + beverage);
+        System.out.printf("That will be $%5.2f\n", price);
+
+        return price;
+    }
+}
---
+++ src/test/java/CoffeeShopTest.java
@@ -0,0 +1,26 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class CoffeeShopTest {
+
+
+    @Test
+    void order_medium_coffee_price() {
+
+        CoffeeShop shop = new CoffeeShop();
+        double price = shop.order(DrinkType.COFFEE, SizeType.MEDIUM);
+
+        assertEquals(price, 4.98, "Wrong price!");
+
+    }
+
+    @Test
+    void order_large_coffee_price() {
+
+        CoffeeShop shop = new CoffeeShop();
+        double price = shop.order(DrinkType.COFFEE, SizeType.LARGE);
+
+        assertEquals(price, 6.0, "Wrong price!");
+
+    }
+}
