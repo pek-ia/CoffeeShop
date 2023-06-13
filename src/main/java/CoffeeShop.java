f003e54862bf8342f19cc779f90cb67fc2ce3261
Paul Kimball <pekimball@interfaceassociates.net>
2023-06-12 22:26:27 -0400

    new class for main

--- src/main/java/CoffeeShop.java
+++ src/main/java/CoffeeShop.java
@@ -1,33 +1,8 @@
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class CoffeeShop {
 
-    public static void main(String[] args) {
-        CoffeeShop shop = new CoffeeShop();
-        shop.order(DrinkType.COFFEE, SizeType.MEDIUM);
-    }
-
     private ArrayList<Beverage> beverageList = new ArrayList<>();
-
-    public CoffeeShop() {
-        this.beverageList.add( new Beverage(SizeType.SMALL, 8.00, DrinkType.COFFEE));
-        this.beverageList.add( new Beverage(SizeType.MEDIUM, 4.98, DrinkType.COFFEE));
-        this.beverageList.add( new Beverage(SizeType.LARGE, 6.00, DrinkType.COFFEE));
-        this.beverageList.add( new Beverage(SizeType.SMALL, 180.00, DrinkType.TEA));
-        this.beverageList.add( new Beverage(SizeType.MEDIUM, 3.00, DrinkType.TEA));
-        this.beverageList.add( new Beverage(SizeType.LARGE, 6.00, DrinkType.TEA));
-    }
-
-
-    public double findPrice(SizeType size, DrinkType type) {
-        double value = 0.0;
-        for (Beverage b : beverageList) {
-            if (b.getSize() == size && b.getType() == type) value = b.getPrice();
-        }
-        return value;
-    }
-
 
 
     double order(DrinkType beverage, SizeType size) {
@@ -40,5 +15,26 @@ public class CoffeeShop {
         System.out.printf("That will be $%5.2f\n", price);
 
         return price;
+    }
+
+
+
+
+    public double findPrice(SizeType size, DrinkType type) {
+        double value = 0.0;
+        for (Beverage b : beverageList) {
+            if (b.getSize() == size && b.getType() == type) value = b.getPrice();
+        }
+        return value;
+    }
+
+
+    public CoffeeShop() {
+        this.beverageList.add( new Beverage(SizeType.SMALL, 8.00, DrinkType.COFFEE));
+        this.beverageList.add( new Beverage(SizeType.MEDIUM, 4.98, DrinkType.COFFEE));
+        this.beverageList.add( new Beverage(SizeType.LARGE, 6.00, DrinkType.COFFEE));
+        this.beverageList.add( new Beverage(SizeType.SMALL, 180.00, DrinkType.TEA));
+        this.beverageList.add( new Beverage(SizeType.MEDIUM, 3.00, DrinkType.TEA));
+        this.beverageList.add( new Beverage(SizeType.LARGE, 6.00, DrinkType.TEA));
     }
 }
