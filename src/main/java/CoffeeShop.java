26fd0080d03778678e442c38a67eba48de5f6a58
Paul Kimball <pekimball@interfaceassociates.net>
2023-06-12 22:32:56 -0400

    Refactored pricing to PriceDB class

--- src/main/java/CoffeeShop.java
+++ src/main/java/CoffeeShop.java
@@ -2,13 +2,11 @@ import java.util.ArrayList;
 
 public class CoffeeShop {
 
-    private ArrayList<Beverage> beverageList = new ArrayList<>();
-
-
     double order(DrinkType beverage, SizeType size) {
         double price = 0.0;
 
-        price = findPrice(size, beverage);
+        PriceDB db = new PriceDB();
+        price = db.findPrice(size, beverage);
 
 
         System.out.println("you ordered a " + size + " " + beverage);
@@ -17,24 +15,4 @@ public class CoffeeShop {
         return price;
     }
 
-
-
-
-    public double findPrice(SizeType size, DrinkType type) {
-        double value = 0.0;
-        for (Beverage b : beverageList) {
-            if (b.getSize() == size && b.getType() == type) value = b.getPrice();
-        }
-        return value;
-    }
-
-
-    public CoffeeShop() {
-        this.beverageList.add( new Beverage(SizeType.SMALL, 8.00, DrinkType.COFFEE));
-        this.beverageList.add( new Beverage(SizeType.MEDIUM, 4.98, DrinkType.COFFEE));
-        this.beverageList.add( new Beverage(SizeType.LARGE, 6.00, DrinkType.COFFEE));
-        this.beverageList.add( new Beverage(SizeType.SMALL, 180.00, DrinkType.TEA));
-        this.beverageList.add( new Beverage(SizeType.MEDIUM, 3.00, DrinkType.TEA));
-        this.beverageList.add( new Beverage(SizeType.LARGE, 6.00, DrinkType.TEA));
-    }
 }
---
+++ src/main/java/PriceDB.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class PriceDB {
+
+    private ArrayList<Beverage> beverageList = new ArrayList<>();
+
+
+    public double findPrice(SizeType size, DrinkType type) {
+        double value = 0.0;
+        for (Beverage b : beverageList) {
+            if (b.getSize() == size && b.getType() == type) value = b.getPrice();
+        }
+        return value;
+    }
+
+
+    public PriceDB() {
+        this.beverageList.add( new Beverage(SizeType.SMALL, 8.00, DrinkType.COFFEE));
+        this.beverageList.add( new Beverage(SizeType.MEDIUM, 4.98, DrinkType.COFFEE));
+        this.beverageList.add( new Beverage(SizeType.LARGE, 6.00, DrinkType.COFFEE));
+        this.beverageList.add( new Beverage(SizeType.SMALL, 180.00, DrinkType.TEA));
+        this.beverageList.add( new Beverage(SizeType.MEDIUM, 3.00, DrinkType.TEA));
+        this.beverageList.add( new Beverage(SizeType.LARGE, 6.00, DrinkType.TEA));
+    }
+
+}
